34255a3e1c4ea572cd6e4fb315e08364c58a12bf
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-06-25 21:39:15 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,11 +1,6 @@
 year=int(input())
-if(year%4 == 0):
-  if(year%100 == 0):
-    if(year%400 == 0):
-      print("Yes")
-    else:
-      print("No")
-  else:
-    print("Yes")
+if(year%4 == 0  or year%400 == 0 and year%100 == 0):
+  print("Yes")
 else:
   print("No")
+
