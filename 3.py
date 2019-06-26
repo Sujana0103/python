c75f078ca76fbe2032b36b83122df419cdb83222
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-06-26 15:11:50 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,8 +1,5 @@
 ch=input()
-if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-  if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
-    print("Vowel")
-  else:
-    print("Consonant")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+  print("Vowel")
 else:
-  print("invalid")
+  print("Consonant")
