86fc8456a289d6555d701d9cd1d29f67cfd25a38
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-06-30 22:25:18 +0530

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,16 @@
+num=int(input())
+sum-0
+count=0
+temp=num
+while temp>0:
+  count=count+1
+  temp=temp//10
+temp=num
+while temp>0:
+  reminder=temp%10
+  sum=sum+(reminder**count)
+  temp//=10
+if num==sum:
+  print("yes")
+else:
+  print("no")
