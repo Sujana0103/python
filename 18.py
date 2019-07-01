ffade3790fb1128544f91cc3e672fe435e790f96
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-01 15:41:11 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,10 +1,10 @@
-dig1,dig2=map(int,input().split())
-for num in range(dig1+1,dig2):
+no1,no2=map(int,input().split())
+for num in range(no1+1,no2):
   sum=0
-  temp=num
-  while temp>0:
-    digit=temp%10
+  temp0=num
+  while temp0>0:
+    digit=temp0%10
     sum+=digit**3
-    temp//=10
+    temp0//=10
   if num==sum:
     print(num,end=' ')
