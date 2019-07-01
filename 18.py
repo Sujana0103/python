bf7bab09046043dd5325f00e8c2320db802ae757
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-01 15:35:27 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,10 @@
+dig1,dig2=map(int,input().split())
+for num in range(dig1+1,dig2):
+  sum=0
+  temp=num
+  while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+  if num==sum:
+    print(num,end='')
