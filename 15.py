90e74bc4083f549d8fad1e2bbdcb91fcdf470805
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-01 14:34:18 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,4 +1,4 @@
-number1,number2=map(int,input().split())
-for i in range(number1+1,number2):
+digit1,digit2=map(int,input().split())
+for i in range(digit1+1,digit2):
   if((i%2)==0):
     print(i,end='')
