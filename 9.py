e2f2dc15cafc3d1a46f7711bae5998acbe7ba89e
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-03 14:39:33 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,6 +1,6 @@
-n,k=map(int,input().split())
-l=list(map(int,input()).split())
+n1,k2=map(int,input().split())
+l=list(map(int,input().split()))
 sum=0
-for i in range(k):
+for i in range(k2):
   sum=sum+l[i]
 print(sum)
