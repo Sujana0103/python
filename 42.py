284f6efa2212494f45273bb0df2c6a28393ff549
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-04 11:46:04 +0530

    Update 42.py

--- 42.py
+++ 42.py
@@ -1,5 +1,5 @@
 str1,str2=map(str,input().split())
-if (len(str1)>len(str2)) or (len(str1)==len(str2)):
-  print(str1)
-else:
+if (len(str1)<len(str2)) or (len(str1)==len(str2)):
   print(str2)
+else:
+  print(str1)
