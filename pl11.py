f39ada4bb7405f8c8a7efe22a19e8c882e5b5f86
Sujana0103 <51949286+Sujana0103@users.noreply.github.com>
2019-07-24 19:33:02 +0530

    Update pl11.py

--- pl11.py
+++ pl11.py
@@ -1,9 +1,9 @@
-a=int(input())
-x=0
+intg=int(input())
+n=0
 arr=[]
-for i in range(1,a+1):
+for i in range(1,intg+1):
 	arr.append(i)
 for i in range(len(arr)):
 	for i in range(i+1,len(arr)):
-		x+=1
-print(x)
+		n+=1
+print(n)
